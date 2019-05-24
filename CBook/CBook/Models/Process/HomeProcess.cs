ba2626b7af8fd145e3c4a8fca50114b2c26cb403
VU BAO KHANG <khang.t174997@vanlanguni.vn>
2019-05-24 10:52:06 +0700

    Code

--- CBook/CBook/Models/Process/HomeProcess.cs
+++ CBook/CBook/Models/Process/HomeProcess.cs
@@ -44,7 +44,10 @@ namespace CBook.Models.Process
         /// </summary>
         /// <param name="key">string</param>
         /// <returns>List</returns>
-        
+        public List<Sach> Search(string key)
+        {
+            return db.Saches.Where(x => x.TenSach.Contains(key)).OrderBy(x => x.TenSach).ToList();
+        }
 
     }
 }
